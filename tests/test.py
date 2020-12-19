a81763aaeb05faf5efb3eeac79a7136f94c20818
america <dreamers.ball66@gmail.com>
2020-12-19 21:36:58 +0900

    add test.py

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+import unittest
+
+class SampleTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_sample_method(self):
+        expected = 1
+        actual = 1
+        self.assertEqual(expected, actual)
+
+if __name__ == "__main__":
+    unittest.main()
