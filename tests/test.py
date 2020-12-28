d1143db6f3adf0eb166ecdc8970d4b6c212d2df2
america <dreamers.ball66@gmail.com>
2020-12-29 00:33:18 +0900

    テストコード　ケース１の実装

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,20 @@
 import unittest
+from mock import MagicMock
+import patrush
 
 
 class TestPatrush(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_on_message(self):
-        expected = 'にゃーん'
-        actual = 'にゃーん'
+    # ケース１　BOTなら無視する
+    # returnでNoneが返却されることを確認する
+    def test_on_message_case01(self):
+        case01Mock = MagicMock()
+        case01Mock.author.bot = True
+        actual = patrush.on_message(case01Mock)
+        expected = None
+
         self.assertEqual(expected, actual)
 
 
